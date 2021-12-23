7466e4e3932280dad2467b4a9dbc7ed313cc76e8
email <ivan.robak@gmail.com>
2021-12-23 11:52:35 +0200

    js

---
+++ js/script.js
@@ -0,0 +1,73 @@
+"use strict";
+
+// let number = 5;
+
+// number = 333;
+// console.log(number);
+
+// const leftWiedhBorder = 7;
+//  console.log(leftWiedhBorder);
+
+
+//  const obj = {
+//      h:65
+//  };
+
+//  obj.h = 77;
+//  console.log(obj.h); 
+
+
+// console.log(4/0);
+// console.log(-4/0);
+
+// console.log('string' * 9);
+
+
+// let und;
+// console.log(und);
+
+
+// const obj = {
+//     name:"john",
+//     age:25,
+//     isMarried:false
+// };
+
+// console.log(obj.name);
+
+// alert ( "Hello");
+
+// const result = confirm("Are you here");
+// console.log(result);
+
+
+// const answer = +prompt("Вам є 18років?");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt("Ваше імя?","" );
+// answers[1] = prompt("Ваше фамілія?","");
+// answers[2] = prompt("Скільки вам років?","");
+
+// console.log(answers);
+
+
+// const category = 'toys';
+
+// console.log(`http://someurl/${category}/5`);
+ 
+
+// const user = "Ivan";
+
+// alert(`Привіт , ${user}`);
+
+
+
+
+
+
+
+
+
+ 
